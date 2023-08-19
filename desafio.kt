48d501f0128df772a328318f2cf18704fa16d608
Wadrian Araujo <83624647+WadrianAraujo@users.noreply.github.com>
2023-08-19 17:24:40 -0400

    update code

--- desafio.kt
+++ desafio.kt
@@ -1,11 +1,10 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
+data class Usuario(val nome: String, val id: Int)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
@@ -17,6 +16,12 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
+    val exitAplication: Boolean == false
+    
+    while(exitAplication == false){
+
+
+    }
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
